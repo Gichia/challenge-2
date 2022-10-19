1182458a28e028c183c828c70b829f1a4d7a0b6a
Gichia <petergichia35@gmail.com>
2022-10-19 10:57:07 +0300

    create the duplicates challenge file and add the class with documentation

---
+++ duplicates.py
@@ -0,0 +1,54 @@
+"""
+Contains a class Duplicate with a method that recieves a list
+of numbers and returns an output list with diplicates from the original.
+
+See method duplicates description
+
+Classes:
+
+    Duplicate
+
+Functions:
+
+    main(input_list):
+        recieves the input list and returns list with any duplicates
+
+Misc variables:
+
+    None
+"""
+
+class Duplicate:
+    """
+    The duplicate class to implement method to return duplicates in a list
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    get_duplicates(input_list: list[int]):
+        Returns a list of duplicate items from the input list
+    """
+
+    @classmethod
+    def get_duplicates(cls, input_list: list[int]) -> list[int]:
+        """
+        Returns a list of any duplicate items from the input list
+
+        Parameters:
+            input_list (list[int]):
+                The input list
+
+        Returns:
+            output_list (list[int]):
+                The output list containing any duplicates
+
+        Raises:
+            None
+        """
+
+        pass
