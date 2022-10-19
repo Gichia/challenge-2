bd2b916949e04242fff3c65afbf85ec20d086596
Gichia <petergichia35@gmail.com>
2022-10-19 13:04:36 +0300

    create the monkey patching challenge file

---
+++ monkey_patching.py
@@ -0,0 +1,18 @@
+"""
+Contains example of monkey patching in python.
+
+Monkey patching when we dynamically alter the behavior of a
+piece of code at run time
+
+Classes:
+
+    Employee
+
+Functions:
+
+    None
+
+Misc variables:
+
+    None
+"""
