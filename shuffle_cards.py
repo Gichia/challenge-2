7fde419a82dc8ad7d24dfc8ac2ac659ffc95cadd
Gichia <petergichia35@gmail.com>
2022-10-19 11:22:41 +0300

    create file for the shuffle card challenge and docs

---
+++ shuffle_cards.py
@@ -0,0 +1,36 @@
+"""
+Contains methods that can shuffle items in a list 
+
+Classes:
+
+    None
+
+Functions:
+
+    shuffle(input_list: list[str | int]):
+        recieves the input list and returns list of the items shuffled
+
+Misc variables:
+
+    None
+"""
+
+from typing import List, Union
+
+def shuffle(input_list: List[Union[int, str]]):
+    """
+    Recieves an input list and returns a shuffled version of it
+
+    Parameters:
+        input_list (list[int | str]):
+            The input list to be shufled
+
+    Returns:
+        output_list (list[int | str]):
+            The shuffled version of the input list
+
+    Raises:
+        None
+    """
+
+    pass
